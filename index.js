39c54263c81b5d0cdd4f0010c18182bc63a70615
jessebillard <jessebillard@mac.com>
2018-06-13 16:14:09 -0400

    adds scrolling to list, adds create new note function

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 document.addEventListener('DOMContentLoaded', init)
 
+    const noteListContainer = document.querySelector('#note-list-container')
     const noteList = document.querySelector('#note-list')
-    // const buttons = document.querySelectorAll('.button')
-    // const buttons = document.querySelectorAll('.button')
     const notePanel = document.querySelector('#note-panel')
+    const notePanelHeading = document.querySelector('#note-panel-heading')
+    const newNoteBtn = document.querySelector("#new-note-button")
+    const noteForm = document.querySelector(".form")
+   
 
     function init() {
         renderNoteList()
-        
+        createNewNote()
     }
 
     function renderNote(id) {
         Adapter.getOne(id).then(note => {
-            notePanel.innerHTML = `<form data-id="${note.id}">
+            notePanelHeading.innerText = "Edit Note!"
+            noteForm.dataset.noteId = note.id;
+            noteForm.innerHTML = `
                 <label id="title">Title:</label>
                 <input name="title" type="text" value="${note.title}">
                 <br>
                 <label id="body">Body:</label>
                 <textarea class="submission-field" name="body" type="text-area">${note.body}</textarea>
-                <button id="submit-Btn">Submit!</button>
-            </form>`
+                <button id="submit-Btn">Submit!</button>`
         })
     }
 
@@ -33,36 +37,60 @@ document.addEventListener('DOMContentLoaded', init)
             <button class="delete" data-id="${note.id}">Delete!</button>
             <button class="edit" data-id="${note.id}">Edit!</button>
             </li>`
-        //    console.log(document.querySelectorAll('.button'))
+        
         }) 
         
        )  
     }
  
 
-    noteList.addEventListener('click', (e) => {
+    function createNewNote() {
+        notePanelHeading.innerText = "Create New Note!"
+        noteForm.dataset.id = ""
+        noteForm.innerHTML = `
+        <label id="title">Title:</label>
+        <input name="title" type="text" value="">
+        <br>
+        <label id="body">Body:</label>
+        <textarea class="submission-field" name="body" type="text-area"></textarea>
+        <button id="submit-Btn">Submit!</button>`
+    }
 
+    noteListContainer.addEventListener('click', (e) => {
+       
+        if (e.target.id = "new-note-button") {
+            createNewNote()
+        }
         if (e.target.className === "delete") {
             Adapter.deleteNote(e.target.dataset.id)
             e.target.parentElement.remove()
         } else if (e.target.className === "edit"){ 
             renderNote(e.target.dataset.id)
-            
         } 
            
     })
+    
 
     notePanel.addEventListener('submit', (e) => {
-        const updatedData = {
-            "title": e.target.title.value,
-            "body": e.target.body.value
+        // debugger;
+        if (e.target.dataset.noteId) {
+            const updatedData = {
+                "title": e.target.title.value,
+                "body": e.target.body.value
+            }
+            Adapter.editNote(noteForm.dataset.noteId, updatedData)
+            Adapter.renderNoteList()
+        } 
+        else if (!e.target.dataset.noteId) {
+            const newData = {
+                "title": e.target.title.value,
+                "body": e.target.body.value
+            }
+            Adapter.createNote(newData)
+            Adapter.renderNoteList()
         }
-        Adapter.editNote(e.target.dataset.id, updatedData)
-        Adapter.renderNoteList()
     })
-    
 
-    
 
 
 
